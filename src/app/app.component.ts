0224bfe316963f0a71f2148862a3bba92ebc1670
Taner Saydam <taner_and_you@hotmail.com>
2025-03-03 19:05:14 +0300

    I added demo for the grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject, resource, signal } from '@angular/core';
+import { FlexiGridModule } from '../../projects/flexi-gird/src/lib/modules/flexi-grid.module';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FlexiGridModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'flexi-grid';
+  constructor(){
+    debugger
+  }
+  readonly result = resource({
+    loader: async() => {
+      const res = await lastValueFrom(this.#http.get<any[]>("https://jsonplaceholder.typicode.com/users"));
+      return res;
+    }
+  });
+
+  readonly data = computed(() => this.result.value() ?? []);
+  readonly loading = computed(() => this.result.isLoading());
+
+  readonly #http = inject(HttpClient);
 }
